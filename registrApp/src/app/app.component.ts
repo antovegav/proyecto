8661f717f16b83c3a07c0ae5be44bac36637031b
antovegav <anto.vegav@duocuc.cl>
2023-08-25 05:17:30 -0400

    v 1.0.1

---
+++ registrApp/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [IonicModule, RouterLink, RouterLinkActive, CommonModule],
+})
+export class AppComponent {
+  // ARREGLO CON LAS PAGINAS QUE SE MOSTRARAN EN EL MENU
+  public appPages = [
+    { title: 'Inicio', url: '/folder/RegistrApp', icon: 'home' },
+    { title: 'Asistencia', url: '/folder/Asistencia', icon: 'list' },
+    { title: 'Codigo QR', url: '/folder/Codigo QR', icon: 'qr-code' },
+    { title: 'Editar Usuario', url: '/folder/Editar Usuario', icon: 'person-circle' },
+    { title: 'Ingresar', url: 'register', icon: 'log-in' },
+  ];
+
+  // ARREGLO DE LAS ETIQUETAS QUE SE MOSTRARAN EN EL MENU
+  public labels = [ ];
+  constructor() {}
+}
---
+++ registrApp/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/inbox',
+    pathMatch: 'full',
+  },
+  {
+    path: 'folder/:id',
+    loadComponent: () =>
+      import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./users/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./users/register/register.page').then( m => m.RegisterPage)
+  },
+];
